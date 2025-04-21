4589479de3232c4b6bb8fac0c18669017d11ff74
Brian <briansastre159@gmail.com>
2025-04-21 19:54:08 -0300

    clase de python 21/4/2025

---
+++ Tecnicatura-UTN/python/CalificaTudia.py
@@ -0,0 +1,7 @@
+# Ejercicio 1 - Califica tu día
+
+# Preguntamos al usuario cómo estuvo su día
+calificacion = input("¿Cómo estuvo tu día (1 al 10)? ")
+
+# Mostramos la calificación con el formato pedido
+print("Mi día estuvo de:", calificacion)
---
+++ Tecnicatura-UTN/python/InformacionDelLibro.py
@@ -0,0 +1,8 @@
+# Ejercicio 2 - Información de un libro
+
+# Pedimos el título y el autor
+titulo = input("Proporciona el titulo: ")
+autor = input("Proporciona el autor: ")
+
+# Mostramos la frase final
+print(f"{titulo} fue escrito por {autor}")
